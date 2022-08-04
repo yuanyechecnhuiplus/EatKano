5b1ce469e07ba5f8f9927880e797c81c343300b6
yuanyechecnhuiplus <110607493+yuanyechecnhuiplus@users.noreply.github.com>
2022-08-04 21:22:30 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -502,10 +502,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return I18N['text-level-1'];
-        if (cps <= 8) return I18N['text-level-2'];
-        if (cps <= 10)  return I18N['text-level-3'];
-        if (cps <= 15) return I18N['text-level-4'];
+        if (cps <= 4) return I18N['text-level-1'];
+        if (cps <= 6) return I18N['text-level-2'];
+        if (cps <= 8)  return I18N['text-level-3'];
+        if (cps <= 10) return I18N['text-level-4'];
         return I18N['text-level-5'];
     }
 
